9cc999a494af293cc8ec7b3254229cf99c012abe
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-07-29 23:13:38 -0400

    Install bootstrap & start styling project index view

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   devise_for :users# , :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root "users#show"
+  root "groups#index"
 end
