83f9e88efd825c31b52bfd3a9009ab3ed7ea0baa
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-07-31 10:21:58 -0400

    Change projects page to my projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[show edit update destroy]
-  before_action :authenticate_user!, only: %i[new create edit update]
+  before_action :authenticate_user!, only: %i[new create edit update index]
 
   # GET /projects or /projects.json
   def index
