0e6f0a74b6b05291d49e081b3c4c6fce1aba780c
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-08-02 20:55:50 -0400

    Add image format validation to group icon_url

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :icon_url, presence: true, url: true
+  validates :icon_url, format: { with: /\.(png|jpg)\Z/i, message: "must end in .png or .jpg!" }
   has_many :grouped_projects
   has_many :projects, through: :grouped_projects
 
